58502f7c6262d97b9a52cdf5671e4bb26a49ee71
Ajay <er.rokade@gmail.com>
2023-08-03 16:51:28 +0530

    i have created second program

---
+++ TestProject/src/AjaySecondProgram.java
@@ -0,0 +1,19 @@
+
+public class AjaySecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String name="ajayrokade";
+		String role="Software Tester";
+		name.charAt(5);
+		name.indexOf("y");
+		name.concat(role);
+		System.out.println(name.concat(role));
+		System.out.println(name.charAt(3));
+		System.out.println(role.toUpperCase());
+
+	}
+
+}
