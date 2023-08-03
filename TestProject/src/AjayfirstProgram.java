bec766a8052714d74b15c67a75b63f94fbe2e268
Ajay <er.rokade@gmail.com>
2023-08-03 16:21:51 +0530

    make change in code like ajay

---
+++ TestProject/src/AjayfirstProgram.java
@@ -0,0 +1,12 @@
+
+public class AjayfirstProgram {
+
+public static void main(String[] args) {
+	
+	
+	
+	
+	System.out.println(" i have succesfully send the code on github repository");
+	System.out.println("HI git hub");
+}
+}
