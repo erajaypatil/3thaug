9f28edf13cd4492b16eb5d1c24130c7bad878ac8
Ajay <er.rokade@gmail.com>
2023-08-03 16:46:43 +0530

    added main change in program

--- TestProject/src/AjayfirstProgram.java
+++ TestProject/src/AjayfirstProgram.java
@@ -9,5 +9,6 @@ public static void main(String[] args) {
 	System.out.println(" i have succesfully send the code on github repository");
 	System.out.println("HI git hub");
 	System.out.println("hello this is jay again i am enter into the github welcome ");
+	System.out.println("this is the main change in program");
 }
 }
